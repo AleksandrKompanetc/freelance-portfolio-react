03be941deac4759b6f4fa0c1634d3a0ba289e389
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-26 23:33:49 +0300

    adding activeLink

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,20 @@
+import { NavLink } from 'react-router-dom';
+
 import sun from '../../img/icons/sun.svg';
 import moon from '../../img/icons/moon.svg';
 import './style.css';
 
 function Navbar() {
+  const activeLink = 'nav-list__link nav-list__link--active';
+  const normalLink = 'nav-list__link';
+
   return (
     <nav className="nav">
       <div className="container">
         <div className="nav-row">
-          <a href="./index.html" className="logo"><strong>Freelancer</strong> Portfolio</a>
+          <NavLink to='/' className="logo">
+            <strong>Freelancer</strong> Portfolio
+          </NavLink>
 
           <button className="dark-mode-btn">
             <img src={sun} className="dark-mode-btn__icon" width="20px" alt="Light mode" />
@@ -15,14 +22,26 @@ function Navbar() {
           </button>
 
           <ul className="nav-list">
-            <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Projects</a></li>
-            <li className="nav-list__item"><a href="./skills.html" className="nav-list__link">Skills</a></li>
-            <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+            <li className="nav-list__item">
+              <NavLink to='/' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink to='/projects' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink to='/contacts' className={({ isActive }) => isActive ? activeLink : normalLink}>
+                Contacts
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
