dabeefe07a308c1175864099905d2e3169460113
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-27 17:47:14 +0300

    last commit

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -3,23 +3,33 @@ import {useLocalStorage} from './../../utils/useLocalStorage';
 import sun from './sun.svg'
 import moon from './moon.svg';
 import './style.css';
+import detectDarkMode from '../../utils/detectDarkMode';
 
 const BtnDarkMode = () => {
   // const [darkMode, setDarkMode] = useState('light');
 
-  const [darkMode, setDarkMode] = useLocalStorage('darkMode', 'light');
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
 
   const btnRef = useRef(null);
 
   useEffect(() => {
     if (darkMode === 'dark') {
       document.body.classList.add('dark');
-      btnRef.current.classList.add('dark-mode-btn--active');
+      // btnRef.current.classList.add('dark-mode-btn--active');
     } else {
       document.body.classList.remove('dark');
-      btnRef.current.classList.remove('dark-mode-btn--active');
+      // btnRef.current.classList.remove('dark-mode-btn--active');
     }
   }, [darkMode]);
+
+  useEffect(() => {
+    window
+          .matchMedia('(prefers-color-scheme: dark)')
+          .addEventListener('change', (event) => {
+            const newColorScheme = event.matches ? 'dark' : 'light';
+            setDarkMode(newColorScheme);
+          })
+  })
 
   const toggleDarkMode = () => {
     setDarkMode((currentValue) => {
@@ -27,8 +37,11 @@ const BtnDarkMode = () => {
     });
   }
 
+  const btnNormal = 'dark-mode-btn';
+  const btnActive = 'dark-mode-btn dark-mode-btn--active';
+
   return (
-    <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
+    <button ref={btnRef} className={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
       <img src={sun} className="dark-mode-btn__icon" width="20px" alt="Light mode" />
       <img src={moon} className="dark-mode-btn__icon" width="20px" alt="Dark mode" />
     </button>
